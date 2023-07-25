31a8a6f245caf46e7ba1276a393ebf60d62f3a90
Idriss <idrissbenguezzou@gmail.com>
2023-07-25 21:21:50 +0200

    feat: display serie from history && handle backClick

--- videmo/src/pages/History/History.jsx
+++ videmo/src/pages/History/History.jsx
@@ -6,10 +6,12 @@ import DeleteOutlineIcon from '@mui/icons-material/DeleteOutline';
 // Components
 import Header from "../../components/Header/Header";
 import VideoPlayer from "../../components/VideoPlayer/VideoPlayer";
+import SerieDisplay from "../../components/SeriesDisplay/SeriesDisplay";
 
 // Services
 import HistoryApi from "../../services/api/track/HistoryApi";
 import TrackApi from "../../services/api/track/TrackApi";
+import FolderManager from "../../utilities/folderManager/FolderManager";
 
 // Styles
 import styles from "./History.module.scss";
@@ -19,11 +21,15 @@ function History() {
     // Services initialization
     const [historyApi] = useState(() => new HistoryApi());
     const [trackApi] = useState(() => new TrackApi());
+    const [folderManager] = useState(() => new FolderManager());
 
     // State initialization
     const [history, setHistory] = useState([]);
     const [searchValue, setSearchValue] = useState("");
     const [selectedEntry, setelectedEntry] = useState(null); // To store the selected entry
+    const [showSerieDisplay, setShowSerieDisplay] = useState(false); // To show the serie display
+    const [showVideoPlayer, setShowVideoPlayer] = useState(false); // To show the video player
+    const [episodes, setEpisodes] = useState([]); // To store the episodes of the selected serie
 
 
     useEffect(() => {
@@ -31,17 +37,27 @@ function History() {
         historyApi.retrieveAllEpisodeAndSerieHistory()
             .then((data) => setHistory(data))
             .catch((error) => console.log(error));
-    }, [historyApi]);
+    }, [historyApi, showSerieDisplay]);
 
 
     // Handle click on serie name to show VideoPlayer
     const handleSerieNameClick = (serie) => {
         setelectedEntry(serie);
+        setShowVideoPlayer(true);
     };
 
     // Handle click on serie image to show SerieDisplay
-    const handleSerieImageClick = (serie) => {
-        setelectedEntry(serie);
+    const handleSerieImageClick = async (entry) => {
+        try {
+            const data = await folderManager.retrieveFilesInFolder(entry.serie.link);
+            const retrievedEpisodes = await trackApi.readAllEpisodesBySerieLink(entry.serie.link);
+            setEpisodes(trackApi.mapSerieEpisodeWithDatabaseEpisode(data, retrievedEpisodes));
+
+            setelectedEntry(entry);
+            setShowSerieDisplay(true);
+        } catch (error) {
+            console.log(error);
+        }
     };
 
     // TODO : define as utils function
@@ -60,6 +76,8 @@ function History() {
 
     const handleCloseVideoPlayer = (playedTime, episodeFinished) => {
         setelectedEntry(null);
+        setShowVideoPlayer(false);
+
         if (episodeFinished) {
             const updatedEpisode = { ...selectedEntry.episode, viewed: !selectedEntry.serie.currentEpisode.viewed, played_time: 0 };
             trackApi.addEpisodeToViewed(updatedEpisode, updatedEpisode);
@@ -104,51 +122,62 @@ function History() {
         }
     };
 
+    const handleBackClick = () => {
+        setShowSerieDisplay(false);
+        setelectedEntry(null);
+        setEpisodes([]);
+    };
+
     return (
         <div className={styles.history}>
             <Header
                 title="Historique"
+                onBack={selectedEntry && showSerieDisplay ? handleBackClick : null}
                 onSearch={setSearchValue}
             />
 
-            <div className={styles.content}>
-                {filterHistory.map((entry, index) => {
-                    const currentDateLabel = getDateLabel(entry.history.timestamp);
-                    const prevEntry = index > 0 ? filterHistory[index - 1] : null;
-                    const prevDateLabel = prevEntry ? getDateLabel(prevEntry.history.timestamp) : null;
-                    const isNewDateLabel = currentDateLabel !== prevDateLabel;
+            {!showSerieDisplay ? (
+                <>
+                    <div className={styles.content}>
+                        {filterHistory.map((entry, index) => {
+                            const currentDateLabel = getDateLabel(entry.history.timestamp);
+                            const prevEntry = index > 0 ? filterHistory[index - 1] : null;
+                            const prevDateLabel = prevEntry ? getDateLabel(prevEntry.history.timestamp) : null;
+                            const isNewDateLabel = currentDateLabel !== prevDateLabel;
 
-                    return (
-                        <>
-                            {isNewDateLabel && <p className={styles.dateLabel}>{currentDateLabel}</p>}
-                            <div className={styles.historyCard} key={entry.episode.id}>
-                                <img
-                                    className={styles.cover}
-                                    src={entry.serie.image}
-                                    alt={entry.serie.name}
-                                    onClick={() => handleSerieImageClick(entry.serie)}
-                                />
-                                <div className={styles.info}>
-                                    <h2 className={styles.title} onClick={() => handleSerieNameClick(entry)}>
-                                        {constructTitle(entry.serie)}
-                                    </h2>
-                                    <p className={styles.episodeName}> {entry.episode.name} </p>
-                                </div>
-                                <DeleteOutlineIcon className={styles.deleteIcon} onClick={handleDeleteEpisodeHistory(entry.episode)} />
-                            </div>
-                        </>
-                    );
-                })}
-            </div>
-
-            {selectedEntry && (
-                <div>
-                    {/* Render VideoPlayer or SerieDisplay based on the selectedEntry */}
-                    {/* Pass the necessary props, e.g., link, startTime, onCloseVideoPlayer */}
-                    {/* You can also add a close button or handle closing in a similar way */}
-                    <VideoPlayer link={selectedEntry.episode.link} startTime={selectedEntry.episode.played_time} onCloseVideoPlayer={handleCloseVideoPlayer} />
-                    {/* <SerieDisplay serie={selectedEntry} onCloseSerieDisplay={() => setelectedEntry(null)} /> */}
-                </div>
+                            return (
+                                <>
+                                    {isNewDateLabel && <p className={styles.dateLabel}>{currentDateLabel}</p>}
+                                    <div className={styles.historyCard} key={entry.episode.id}>
+                                        <img
+                                            className={styles.cover}
+                                            src={entry.serie.image}
+                                            alt={entry.serie.name}
+                                            onClick={() => handleSerieImageClick(entry)}
+                                        />
+                                        <div className={styles.info}>
+                                            <h2 className={styles.title} onClick={() => handleSerieNameClick(entry)}>
+                                                {constructTitle(entry.serie)}
+                                            </h2>
+                                            <p className={styles.episodeName}> {entry.episode.name} </p>
+                                        </div>
+                                        <DeleteOutlineIcon className={styles.deleteIcon} onClick={handleDeleteEpisodeHistory(entry.episode)} />
+                                    </div>
+                                </>
+                            );
+                        })}
+                    </div>
+                    {showVideoPlayer &&
+                        <VideoPlayer link={selectedEntry.episode.link} startTime={selectedEntry.episode.played_time} onCloseVideoPlayer={handleCloseVideoPlayer} />
+                    }
+                </>
+            ) : (
+                <SerieDisplay
+                    serie={selectedEntry.serie}
+                    episodes={episodes}
+                    calledFromExplore={true}
+                    folderContents={[]}
+                />
             )}
         </div>
     );
