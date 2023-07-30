829bafed26b0e6a2a83a74af6713a4cdcb14475f
Idriss <idrissbenguezzou@gmail.com>
2023-07-30 15:27:06 +0200

    feat: frontend source setting component edit name source

--- videmo/src/pages/More/Settings/SourceSettings/SourceSettings.jsx
+++ videmo/src/pages/More/Settings/SourceSettings/SourceSettings.jsx
@@ -4,6 +4,7 @@ import React, { useEffect, useState } from "react";
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import CreateNewFolderOutlinedIcon from '@mui/icons-material/CreateNewFolderOutlined';
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 
 // Services
 import ExtensionsApi from "../../../../services/api/extension/ExtensionApi";
@@ -20,6 +21,8 @@ function SourceSettings() {
 
     // State initialization
     const [extensions, setExtensions] = useState([]);
+    const [editingId, setEditingId] = useState(null);
+    const [editingName, setEditingName] = useState("");
 
     useEffect(() => {
         // Get the list of extensions from the database
@@ -39,25 +42,62 @@ function SourceSettings() {
         // Open the dialog window to select a folder
         folderManager.openDialogWindow()
             .then((path) => {
-                // Create a new extension with the selected folder path
-                extensionApi.createExtension(path, "TEST")
+                // Create a new extension with the selected folder path 
+                extensionApi.createExtension(path, folderManager.retrieveBaseName(path))
                     // Update the array of extensions by adding the new extension
                     .then((link) => setExtensions([...extensions, link]))
                     .catch((error) => console.error(error));
             }).catch((error) => console.error(error));
     };
+
+    const handleEditExtensionName = (id) => {
+        // Update the extension name with the new name
+        const updatedExtensions = extensions.map((extension) =>
+            extension.id === id ? { ...extension, name: editingName } : extension
+        );
+        setExtensions(updatedExtensions);
+        setEditingId(null);
+        setEditingName("");
+    };
+
+    const editExtensionName = (id, name) => {
+        setEditingId(id);
+        setEditingName(name);
+    };
+
     return (
         <>
             <ul className={styles.sourceList}>
-                {extensions?.map((extension) => (
-                    <li key={extension.id} className={styles.sourceItem}>
+                {extensions.map((extension, index) => (
+                    <li key={index} className={styles.sourceItem}>
                         <div className={styles.sourceInfos}>
-                            <p className={styles.sourceName}>{extension.name}</p>
+                            {editingId === extension.id ? ( // If in edit mode, show the input field
+                                <input
+                                    type="text"
+                                    className={styles.editInput}
+                                    value={editingName}
+                                    onChange={(e) => setEditingName(e.target.value)}
+                                    onKeyDown={(e) => e.key === "Enter" && handleEditExtensionName(extension.id)}
+                                    autoFocus // Automatically focus on the input when in edit mode
+                                />
+                            ) : (
+                                <p className={styles.sourceName}>{extension.name}</p> // Show the name if not in edit mode
+                            )}
                             <p className={styles.sourcePath}>{extension.link}</p>
                         </div>
                         <div className={styles.sourceEdit}>
-                            <EditOutlinedIcon className={styles.editIcon} />
-                            <DeleteForeverIcon className={styles.deleteIcon} onClick={() => handleDeleteExtension(extension.id)} />
+                            {editingId !== extension.id ? (
+                                <EditOutlinedIcon
+                                    className={styles.editIcon}
+                                    onClick={() => editExtensionName(extension.id, extension.name)} // Enable edit mode
+                                />
+                            ) : (
+                                <CheckCircleIcon className={styles.editIcon} onClick={() => handleEditExtensionName(extension.id)} />
+                            )}
+                            <DeleteForeverIcon
+                                className={styles.deleteIcon}
+                                onClick={() => handleDeleteExtension(extension.id)}
+                            />
                         </div>
                     </li>
                 ))}
