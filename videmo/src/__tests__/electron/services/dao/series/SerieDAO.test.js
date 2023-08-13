8e46a0b4b50d895bd9ffa46ac59fa59a99b460a0
Idriss <idrissbenguezzou@gmail.com>
2023-08-13 16:12:34 +0200

    feat: serie dao test

---
+++ videmo/src/__tests__/electron/services/dao/series/SerieDAO.test.js
@@ -0,0 +1,333 @@
+const QueryExecutor = require('../../../../../electron/services/sqlite/QueryExecutor');
+const SerieDAO = require('../../../../../electron/services/dao/series/SerieDAO');
+const DataTypesConverter = require('../../../../../electron/utilities/converter/DataTypesConverter.js');
+
+const path = require('path');
+
+describe('SerieDAO', () => {
+    let mockQueryExecutor;
+    let serieDAO;
+
+    const tables = path.join(__dirname, '../../../../../electron/services/sqlite/sql/tables.sql');
+    const drop_tables = path.join(__dirname, '../../../../../electron/services/sqlite/sql/drop_tables.sql');
+
+    beforeAll(async () => {
+        mockQueryExecutor = new QueryExecutor();
+        serieDAO = new SerieDAO();
+
+        serieDAO.queryExecutor = mockQueryExecutor;
+        serieDAO.dataTypesConverter = new DataTypesConverter();
+
+        // Create the Serie table
+        await mockQueryExecutor.executeFile(tables);
+    });
+
+    afterAll(async () => {
+        // Clear the database
+        await mockQueryExecutor.executeFile(drop_tables);
+    });
+
+    it('should insert a serie', async () => {
+        // Arrange
+        const serie = {
+            basename: 'serie',
+            name: 'Serie',
+            description: 'Description',
+            image: 'image.png',
+            link: 'serie',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        // Act
+        await serieDAO.createSerie(serie);
+        const sql = `SELECT * FROM Serie WHERE basename = ?`;
+        const params = [serie.basename];
+        const result = await mockQueryExecutor.executeAndFetchOne(sql, params);
+
+        // Assert
+        expect(result.basename).toBe(serie.basename);
+        expect(result.name).toBe(serie.name);
+        expect(result.description).toBe(serie.description);
+        expect(result.image).toBe(serie.image);
+        expect(result.link).toBe(serie.link);
+        expect(result.extension_id).toBe(serie.extension_id);
+        expect(result.inLibrary).toBe(serie.inLibrary);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE basename = ?`, params);
+    });
+
+    it('should get all series', async () => {
+        // Arrange
+        const serie1 = {
+            basename: 'serie1',
+            name: 'Serie 1',
+            description: 'Description 1',
+            image: 'image1.png',
+            link: 'serie1',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        const serie2 = {
+            basename: 'serie2',
+            name: 'Serie 2',
+            description: 'Description 2',
+            image: 'image2.png',
+            link: 'serie2',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        // Insert series
+        await serieDAO.createSerie(serie1);
+        await serieDAO.createSerie(serie2);
+
+        // Act
+        const result = await serieDAO.getAllSeries();
+
+        // remove the id property
+        result.forEach(serie => delete serie.id);
+
+        // Assert
+        expect(result.length).toBe(2);
+        expect(result[0]).toEqual(serie1);
+        expect(result[1]).toEqual(serie2);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie1.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie2.link]);
+    });
+
+    it('should get a serie by id', async () => {
+        // Arrange
+        const serie = {
+            basename: 'serie',
+            name: 'Serie',
+            description: 'Description',
+            image: 'image.png',
+            link: 'serie',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        // Insert serie
+        await serieDAO.createSerie(serie);
+
+        // Act
+        const result = await serieDAO.getSerieById(1);
+
+        // remove the id property
+        delete result.id;
+
+        // Assert
+        expect(result).toEqual(serie);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie.link]);
+    });
+
+    it('should get a serie by link', async () => {
+        // Arrange
+        const serie = {
+            basename: 'serie',
+            name: 'Serie',
+            description: 'Description',
+            image: 'image.png',
+            link: 'serie',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        // Insert serie
+        await serieDAO.createSerie(serie);
+
+        // Act
+        const result = await serieDAO.getSerieByLink(serie.link);
+
+        // remove the id property
+        delete result.id;
+
+        // Assert
+        expect(result).toEqual(serie);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie.link]);
+    });
+
+    it('should get all series inside library by extension id', async () => {
+        // Arrange
+        const extension = { id: 1, name: 'Extension 1' };
+        const serie1 = {
+            basename: 'serie1',
+            name: 'Serie 1',
+            description: 'Description 1',
+            image: 'image1.png',
+            link: 'serie1',
+            extension_id: extension.id,
+            inLibrary: 1
+        };
+
+        const serie2 = {
+            basename: 'serie2',
+            name: 'Serie 2',
+            description: 'Description 2',
+            image: 'image2.png',
+            link: 'serie2',
+            extension_id: extension.id,
+            inLibrary: 1
+        };
+
+        const serie3 = {
+            basename: 'serie3',
+            name: 'Serie 3',
+            description: 'Description 3',
+            image: 'image3.png',
+            link: 'serie3',
+            extension_id: 2,
+            inLibrary: 1
+        };
+
+        // Insert series
+        await serieDAO.createSerie(serie1);
+        await serieDAO.createSerie(serie2);
+        await serieDAO.createSerie(serie3);
+
+        // Insert extension
+        await mockQueryExecutor.executeAndCommit(`INSERT INTO Extension (id, name) VALUES (?, ?)`, [extension.id, extension.name]);
+
+        // Act
+        const result = await serieDAO.getAllSeriesInLibraryByExtension(extension);
+
+        // remove the id property
+        result.forEach(serie => delete serie.id);
+
+        // Assert
+        expect(result.length).toBe(2);
+        expect(result[0]).toEqual(serie1);
+        expect(result[1]).toEqual(serie2);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie1.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie2.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie3.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Extension WHERE id = ?`, [extension.id]);
+    });
+
+    it('should get all series by category id', async () => {
+        // Arrange
+        const category = { id: 2, name: 'Category 1' };
+        const serie1 = {
+            basename: 'serie1',
+            name: 'Serie 1',
+            description: 'Description 1',
+            image: 'image1.png',
+            link: 'serie1',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        const serie2 = {
+            basename: 'serie2',
+            name: 'Serie 2',
+            description: 'Description 2',
+            image: 'image2.png',
+            link: 'serie2',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        const serie3 = {
+            basename: 'serie3',
+            name: 'Serie 3',
+            description: 'Description 3',
+            image: 'image3.png',
+            link: 'serie3',
+            extension_id: 1,
+            inLibrary: 1
+        };
+
+        // Insert series
+        await serieDAO.createSerie(serie1);
+        await serieDAO.createSerie(serie2);
+        await serieDAO.createSerie(serie3);
+
+        // Insert category
+        await mockQueryExecutor.executeAndCommit(`INSERT INTO Category (id, name) VALUES (?, ?)`, [category.id, category.name]);
+
+        // Insert category_serie
+        await mockQueryExecutor.executeAndCommit(`INSERT INTO SerieCategory (category_id, serie_id) VALUES (?, ?)`, [category.id, 1]);
+        await mockQueryExecutor.executeAndCommit(`INSERT INTO SerieCategory (category_id, serie_id) VALUES (?, ?)`, [category.id, 2]);
+
+        // Act
+        const result = await serieDAO.getSeriesByCategoryId(category.id);
+
+        // remove the id property
+        result.forEach(serie => delete serie.id);
+
+        // Assert
+        expect(result.length).toBe(2);
+        expect(result[0]).toEqual(serie1);
+        expect(result[1]).toEqual(serie2);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie1.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie2.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie3.link]);
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Category WHERE id = ?`, [category.id]);   
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM SerieCategory WHERE category_id = ?`, [category.id]);
+    });
+
+    it('should update library status of a serie', async () => {
+        // Arrange
+        const serie = {
+            basename: 'serie',
+            name: 'Serie',
+            description: 'Description',
+            image: 'image.png',
+            link: 'serie',
+            extension_id: 1,
+            inLibrary: true
+        };
+
+        // Insert serie
+        await serieDAO.createSerie(serie);
+
+        // Act
+        await serieDAO.updateSerieInLibrary(serie.link, serie.inLibrary);
+
+        // Assert
+        const result = await mockQueryExecutor.executeAndFetchAll(`SELECT inLibrary FROM Serie WHERE link = ?`, [serie.link]);
+        expect(result[0].inLibrary).toEqual(1); // 1 = true
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie.link]);
+    });
+
+    it('should delete a serie', async () => {
+        // Arrange
+        const serie = {
+            basename: 'serie',
+            name: 'Serie',
+            description: 'Description',
+            image: 'image.png',
+            link: 'serie',
+            extension_id: 1,
+            inLibrary: true
+        };
+
+        // Insert serie
+        await serieDAO.createSerie(serie);
+
+        // Act
+        await serieDAO.deleteSerieById(1);
+
+        // Assert
+        const result = await mockQueryExecutor.executeAndFetchAll(`SELECT * FROM Serie WHERE link = ?`, [serie.link]);
+        expect(result.length).toEqual(0);
+
+        // Clean up
+        await mockQueryExecutor.executeAndCommit(`DELETE FROM Serie WHERE link = ?`, [serie.link]);
+    });
+});
