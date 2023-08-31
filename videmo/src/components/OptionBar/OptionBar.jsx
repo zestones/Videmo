762e744a02630df4760f391bdf4413e37383d284
Idriss <idrissbenguezzou@gmail.com>
2023-08-31 04:20:58 +0200

    styles: add options buttons && need fix sticky position height

--- videmo/src/components/OptionBar/OptionBar.jsx
+++ videmo/src/components/OptionBar/OptionBar.jsx
@@ -1,8 +1,13 @@
 import React from "react";
 
 // External
+import LabelIcon from '@mui/icons-material/Label';
+import DoneAllIcon from '@mui/icons-material/DoneAll';
+import RemoveDoneIcon from '@mui/icons-material/RemoveDone';
+import DeleteOutlinedIcon from '@mui/icons-material/DeleteOutlined';
 
 // Components
+import Tooltip from "../Tooltip/Tooltip";
 
 // Styles
 import styles from "./OptionBar.module.scss";
@@ -21,8 +26,39 @@ function OptionBar({ series, onClose, checked, onCheck }) {
                     </span>
                     <span className={styles.erase} onClick={() => onClose()}>Effacer</span>
                 </div>
+
                 <div className={styles.optionContainer}>
-        
+                    <Tooltip title="Ajouter à une catégorie" placement="top">
+                        <div className={styles.option}>
+                            <span className={styles.icon}>
+                                <LabelIcon />
+                            </span>
+                        </div>
+                    </Tooltip>
+
+                    <Tooltip title="Marquer comme vu" placement="top">
+                        <div className={styles.option}>
+                            <span className={styles.icon}>
+                                <DoneAllIcon />
+                            </span>
+                        </div>
+                    </Tooltip>
+
+                    <Tooltip title="Marquer comme non vu" placement="top">
+                        <div className={styles.option}>
+                            <span className={styles.icon}>
+                                <RemoveDoneIcon />
+                            </span>
+                        </div>
+                    </Tooltip>
+
+                    <Tooltip title="Supprimer" placement="top">
+                        <div className={styles.option}>
+                            <span className={styles.icon}>
+                                <DeleteOutlinedIcon />
+                            </span>
+                        </div>
+                    </Tooltip>
                 </div>
             </div>
         </div>
