e86344a2ea502d4605d7919e3fd030ad8de78039
Idriss <idrissbenguezzou@gmail.com>
2023-09-18 20:57:53 +0200

    styles: change card style add menu icon

--- videmo/src/components/Card/CategoryCard/CategoryCard.jsx
+++ videmo/src/components/Card/CategoryCard/CategoryCard.jsx
@@ -4,6 +4,7 @@ import React from "react";
 import CheckCircleIcon from '@mui/icons-material/CheckCircle';
 import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
+import MenuIcon from '@mui/icons-material/Menu';
 
 // Styles
 import styles from "./CategoryCard.module.scss";
@@ -17,18 +18,22 @@ function CategoryCard({ category, provided, snapshot, editingCategory, setEditin
             {...provided.dragHandleProps}
             className={styles.categoryItem}
         >
-            {editingCategory?.id === category.id ? (
-                <input
-                    type="text"
-                    className={styles.editInput}
-                    value={editingCategory.name}
-                    onChange={(e) => setEditingCategory({ ...editingCategory, name: e.target.value })}
-                    onKeyDown={(e) => e.key === "Enter" && handleUpdateCategory(category.id)}
-                    autoFocus
-                />
-            ) : (
-                <p className={styles.categoryName}>{category.name}</p>
-            )}
+            <div className={styles.content}>
+                <MenuIcon className={styles.dragIcon} />
+
+                {editingCategory?.id === category.id ? (
+                    <input
+                        type="text"
+                        className={styles.editInput}
+                        value={editingCategory.name}
+                        onChange={(e) => setEditingCategory({ ...editingCategory, name: e.target.value })}
+                        onKeyDown={(e) => e.key === "Enter" && handleUpdateCategory(category.id)}
+                        autoFocus
+                    />
+                ) : (
+                    <p className={styles.categoryName}>{category.name}</p>
+                )}
+            </div>
             <div className={styles.categoryIcons}>
                 {editingCategory?.id === category.id ? (
                     <CheckCircleIcon className={styles.editIcon} onClick={() => handleUpdateCategory(category.id)} />
