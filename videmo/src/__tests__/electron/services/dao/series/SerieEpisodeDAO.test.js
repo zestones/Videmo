fe8805fcf4af56b735bcaeb1f3299674bef153f4
Idriss <idrissbenguezzou@gmail.com>
2023-10-02 23:33:08 +0200

    fix: unit test boolean conversion

--- videmo/src/__tests__/electron/services/dao/series/SerieEpisodeDAO.test.js
+++ videmo/src/__tests__/electron/services/dao/series/SerieEpisodeDAO.test.js
@@ -77,8 +77,8 @@ describe('SerieEpisodeDAO', () => {
         // Assert
         expect(result.name).toBe(episode.name);
         expect(result.link).toBe(episode.link);
-        expect(result.viewed).toBe(serieEpisodeDAO.dataTypesConverter.convertBooleanToInteger(episode.viewed));
-        expect(result.bookmarked).toBe(serieEpisodeDAO.dataTypesConverter.convertBooleanToInteger(episode.bookmarked));
+        expect(result.viewed).toBe(episode.viewed);
+        expect(result.bookmarked).toBe(episode.bookmarked);
         expect(result.played_time).toBe(episode.played_time);
     });
 
