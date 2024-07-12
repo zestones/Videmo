3fc46a9a42b992fa0f4b009b586f0ca0e4aafffd
zestones <idrissbenguezzou@gmail.com>
2024-07-12 18:48:51 +0200

    refactor: source page styles

--- videmo/src/pages/Explore/Source/Source.jsx
+++ videmo/src/pages/Explore/Source/Source.jsx
@@ -1,7 +1,9 @@
 import React, { useEffect, useState, useMemo } from "react";
+import PropTypes from "prop-types";
 
 // External
 import FolderIcon from '@mui/icons-material/Folder';
+import MiscellaneousServicesIcon from '@mui/icons-material/MiscellaneousServices';
 
 // Services
 import ExtensionApi from "../../../services/api/extension/ExtensionApi";
@@ -11,10 +13,10 @@ import SortManager from '../../../utilities/sortManager/SortManager';
 
 // Components
 import Header from "../../../components/Header/Header";
+import Extension from "./Extension/Extension";
 
 // Styles
 import styles from "./Source.module.scss";
-
 
 function Source({ handleSelectedExtension }) {
     // Utilities and services initialization
@@ -22,60 +24,52 @@ function Source({ handleSelectedExtension }) {
     const sortManager = useMemo(() => new SortManager(), []);
 
     // State initialization
-    const [localExtensions, setLocalExtensions] = useState([]);
-    const [remoteExtensions, setRemoteExtensions] = useState([]);
+    const [extensions, setExtensions] = useState({ local: [] });
     const [searchValue, setSearchValue] = useState("");
-    const [activeTab, setActiveTab] = useState("local");
+    const [activeTab, setActiveTab] = useState("source");
 
     useEffect(() => {
         extensionApi.readExtension()
             .then((data) => {
-                setLocalExtensions(data.local)
-                setRemoteExtensions(data.external)
+                console.log(data);
+                data.local = sortManager.sortByName(data.local);
+                data.external = sortManager.sortByName(data.external);
+                setExtensions(data)
             })
             .catch((error) => console.error(error));
-    }, [extensionApi]);
+    }, [extensionApi, sortManager]);
 
     const handleSearch = (value) => setSearchValue(value);
-    const filterExtensions = sortManager.filterByKeyword(searchValue, activeTab === "local" ? localExtensions : remoteExtensions, 'name');
 
     return (
         <>
             <Header title="Explorer" onSearch={handleSearch} />
             <div className={styles.container}>
+
                 <div className={styles.tabs}>
-                    <h3
-                        className={`${styles.tab} ${activeTab === "local" ? styles.active : ""}`}
-                        onClick={() => setActiveTab("local")}
-                    >
-                        Source locale
-                    </h3>
-                    <h3
-                        className={`${styles.tab} ${activeTab === "remote" ? styles.active : ""}`}
-                        onClick={() => setActiveTab("remote")}
-                    >
-                        Source externe
-                    </h3>
+                    <button className={styles.tab + (activeTab === "source" ? ` ${styles.active}` : "")} onClick={() => setActiveTab("source")}>
+                        <FolderIcon className={styles.tabIcon} />
+                        <p className={styles.tabText}>Source</p>
+                    </button>
+                    <button className={styles.tab + (activeTab === "manage" ? ` ${styles.active}` : "")} onClick={() => setActiveTab("manage")}>
+                        <MiscellaneousServicesIcon className={styles.tabIcon} />
+                        <p className={styles.tabText}>Manage</p>
+                    </button>
                 </div>
-                <div className={styles.extensions}>
-                    {filterExtensions.map((extension) => (
-                        <div
-                            key={extension.id}
-                            className={styles.extension}
-                            onClick={() => handleSelectedExtension(extension)}
-                        >
-                            <div className={styles.extensionIcon}>
-                                <FolderIcon className={activeTab === "local" ? styles.localSource : styles.remoteSource} />
-                            </div>
-                            <p className={styles.extensionName}>
-                                {extension.name}
-                            </p>
-                        </div>
-                    ))}
-                </div>
+
+                {activeTab === "source" && (
+                    <Extension
+                        extensions={extensions}
+                        handleSelectedExtension={handleSelectedExtension}
+                    />
+                )}
             </div>
         </>
     );
 }
+
+Source.propTypes = {
+    handleSelectedExtension: PropTypes.func.isRequired,
+};
 
 export default Source;
--- videmo/src/utilities/sortManager/SortManager.js
+++ videmo/src/utilities/sortManager/SortManager.js
@@ -96,6 +96,16 @@ class SortManager {
 
         return object;
     }
+
+    sortByName(data) {
+        data.sort((a, b) => {
+            if (a.name < b.name) return -1;
+            if (a.name > b.name) return 1;
+            return 0;
+        });
+
+        return data;
+    }
 }
 
 export default SortManager;
